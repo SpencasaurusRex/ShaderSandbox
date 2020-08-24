99cc03e08c974e4c0366474e15c7d96b9cd2cf99
SpencasaurusRex <SpencasaurusRex@gmail.com>
2020-08-24 06:15:36 -0400

    Initial commit - Logo

---
+++ Assets/Scripts/ShaderAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderAnimator : MonoBehaviour
+{
+    Material material;
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.SetFloat("T", Time.timeSinceLevelLoad);
+    }
+}
